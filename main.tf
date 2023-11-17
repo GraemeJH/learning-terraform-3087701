084885a9c305f00bad48ab62e574a71b9b80b7cf
GraemeJH <91103726+GraemeJH@users.noreply.github.com>
2023-11-17 15:59:57 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "blog" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group" "blog_http_in" {
+resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
@@ -42,10 +42,20 @@ resource "aws_security_group" "blog_http_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group" "blog_https_out" {
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_out" {
   type        = "egress"
   from_port   = 0
-  to_port     = 00
+  to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
