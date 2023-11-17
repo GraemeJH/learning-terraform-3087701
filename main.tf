31a55cc9547b37165b48c4cf4cba4fe4ca575f03
GraemeJH <91103726+GraemeJH@users.noreply.github.com>
2023-11-17 16:03:29 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "blog" {
   name = "blog"
   description = "allow http"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = data.aws_vpc.blog.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
