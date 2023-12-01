de3200520013ef8f8e451dc54a7dcb9264090107
GraemeJH <91103726+GraemeJH@users.noreply.github.com>
2023-12-01 14:23:51 +1000

    Use mofule for sg

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
@@ -34,7 +34,12 @@ module "blog_sg" {
   version = "4.13.0"
   name    = "blog_new"
   vpc_id  = data.aws_vpc.default.id
-  ingress
+
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules = [all-all]
+  egress_cidr_blocks = ["0.0.0.0/0"]
   
 }
 
